d1ac294034dc96d6776acbaa35347186db35917b
DavidDeodato <159180167+DavidDeodato@users.noreply.github.com>
2025-11-21 07:42:38 -0800

    Add sidebars configuration for documentationAdd Docusaurus sidebar config

---
+++ website/sidebars.js
@@ -0,0 +1,21 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Documentacao',
+      collapsible: false,
+      items: [
+        'index',
+        'entendimento-problema',
+        'solucao',
+        'analise-financeira-tecnica',
+        'impacto',
+        'equipe',
+        'roadmap',
+      ],
+    },
+  ],
+};
+
+module.exports = sidebars;
